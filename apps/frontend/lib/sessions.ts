cf9d263ae3bf2c9a8aa2471c0b853c9878b6be51
Utkarsh Pandey <utk2665@gmail.com>
2026-03-05 12:18:38 +0530

    fix: url.parse() issue fixed by using fetch instead of axios

--- apps/frontend/lib/sessions.ts
+++ apps/frontend/lib/sessions.ts
@@ -1,23 +1,21 @@
 //this file will fetch session data on the server;
 
 import { headers } from "next/headers";
-import axios from "axios";
 
 export async function getSession() {
     const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000";
 
     try {
-
-
-        const res = await axios.get(`${backendUrl}/api/auth/get-session`, {
+        const res = await fetch(`${backendUrl}/api/auth/get-session`, {
             //forward users' cookies manually
             headers: Object.fromEntries(await headers()),
-            withCredentials: true,
+            credentials: "include",
         });
 
-        return res.data;
+        if (!res.ok) return null;
+        return res.json();
     } catch (error) {
-        axios.isAxiosError(error) && console.log(error.response?.data);
+        console.log(error);
         return null;
     }
 }
